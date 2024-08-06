fda70e2aca7ba5bfbbc8758a51037573281f5077
Pranavv361 <pranavvichare361@gmail.com>
2024-08-06 11:31:56 -0500

    Updated index

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const path = require("path");
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
 const PORT = 8000;
+const upload = multer({ dest: "uploads/" });
 
-app.set("view engine11", "ejs");
+app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   return res.render("homepage");
 });
+
+app.post("/upload", (req, res) => {});
 
 app.listen(PORT, () => console.log("Server started at PORT:8000"));
